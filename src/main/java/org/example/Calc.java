066e7dc35a07a58a19d0967507927286e2adc3f3
havegrit <shin_jaean@naver.com>
2023-03-10 16:45:29 +0900

    10 - 10 - 10 - 10 + 10 + 10 - 10 == -10

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -3,13 +3,11 @@ package org.example;
 public class Calc {
     public static int run(String exp){
         exp = exp.replaceAll("- ", "+ -");
-        String[] bits = exp.split(" ");
-        int a = Integer.parseInt(bits[0]);
-        int b = Integer.parseInt(bits[2]);
-        int c = 0;
-        if(bits.length > 3){
-            c = Integer.parseInt(bits[4]);
+        String[] bits = exp.split(" \\+ ");
+        int sum = 0;
+        for (String value : bits){
+            sum += Integer.parseInt(value);
         }
-        return a+b+c;
+        return sum;
     }
 }
